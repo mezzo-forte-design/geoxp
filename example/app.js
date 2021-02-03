e6bc8f2518cb4dbb1ef9fa3ab21657a6456a1705
Giuliano Buratti <gburatti.rc@@gmail.com>
2021-02-03 15:48:01 +0100

    v1.0

--- example/app.js
+++ example/app.js
@@ -58,39 +58,38 @@ import { config } from './config';
 
 
 const geoXp = new GeoXp(config);
-
 window.geoXp = geoXp;
 
 document.getElementById('pos-in-button').addEventListener('click', e => {
-    const position = {
-        coords: {
-            latitude: 45.116177,
-            longitude: 7.742615,
-            accuracy: 1
-        }
-    };
+  const position = {
+    coords: {
+      latitude: 45.116177,
+      longitude: 7.742615,
+      accuracy: 1
+    }
+  };
 
-    geoXp.geo._geoSuccess(position);
+  geoXp.geo._geoSuccess(position);
 });
 
 document.getElementById('pos-out-button').addEventListener('click', e => {
-    const position = {
-        coords: {
-            latitude: 43.116177,
-            longitude: 5.742615,
-            accuracy: 1
-        }
-    };
+  const position = {
+    coords: {
+      latitude: 43.116177,
+      longitude: 5.742615,
+      accuracy: 1
+    }
+  };
 
-    geoXp.geo._geoSuccess(position);
+  geoXp.geo._geoSuccess(position);
 });
 
 document.getElementById('test-button').addEventListener('click', e => {
-    geoXp.audio.test();
+  geoXp.audio.test();
 });
 
 document.getElementById('reset-button').addEventListener('click', e => {
-    geoXp.reload(config);
+  geoXp.reload(config);
 });
 
 document.getElementById('unlock-button').addEventListener('click', e => {
