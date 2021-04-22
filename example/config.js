c5fcabc12ba7b74fa9746b41d2dbdcc8c128dcf8
francescocretti <francesco.cretti@gmail.com>
2021-04-22 20:06:53 +0200

    clean example app

--- example/config.js
+++ example/config.js
@@ -1,62 +1,67 @@
-export const config = {
-  "geo" : {
-    "position" : [
-      {
-        "_id"           : "pos1",
-        "label"         : "pos1",
-        "lat"           : 45.116177,
-        "lon"           : 7.742615,
-        "radius"        : 10000,
-        "deadband"      : 20
-      }
-    ],
-    "default" : {
-      "minAccuracy"     : 100,
-      "posDeadband"     : 10,
-      "playDistance"    : 20,
-      "fetchDistance"   : 1
-    }
-  },
-
-  "audio" : {
-    "sound" : [
-      {
-        "_id"           : "aud1",
-        "label"         : "aud1",
-        "url"           : "./audio/music1.mp3"
-      },
-      {
-        "_id"           : "aud2",
-        "label"         : "aud2",
-        "url"           : "./audio/music2.mp3"
-      }
-    ],
-    "default" : {
-      "test"            : "./audio/system/test.mp3",
-      "silence"         : "./audio/system/silence.mp3",
-      "unclock"         : "./audio/system/unlock.mp3",
-      "visited"         : "./audio/system/visited.mp3"
-    }
-  },
-
-  "itinerary" : [
+// geographic configuration
+const geo = {
+  "positions": [
     {
-      "label"           : "iti1",
-      "enabled"         : true,
-      "overlap"         : false,
-      "spot" : [
+      "_id": "pos1",
+      "label": "pos1",
+      "lat": 45.116177,
+      "lon": 7.742615,
+      "radius": 10000,
+      "deadband": 20
+    }
+  ],
+  "default": {
+    "minAccuracy": 100,
+    "posDeadband": 10,
+    "playDistance": 20,
+    "fetchDistance": 1
+  }
+};
+
+// audio configuration
+const audio = {
+  "sounds": [
+    {
+      "_id": "aud1",
+      "label": "aud1",
+      "url": "./audio/music1.mp3"
+    },
+    {
+      "_id": "aud2",
+      "label": "aud2",
+      "url": "./audio/music2.mp3"
+    }
+  ],
+  "default": {
+    "test": "./audio/system/test.mp3",
+    "silence": "./audio/system/silence.mp3",
+    "unclock": "./audio/system/unlock.mp3",
+    "visited": "./audio/system/visited.mp3"
+  }
+};
+
+// experience configuration
+const experience = {
+  "patterns": [
+    {
+      "label": "pattern1",
+      "enabled": true,
+      "overlap": false,
+      "spot": [
         {
-          "_id"         : "spot1",
-          "position"    : "pos1",
-          "audio"       : "aud1"
+          "_id": "spot1",
+          "position": "pos1",
+          "audio": "aud1"
         },
         {
-          "_id"         : "spot2",
-          "position"    : "pos1",
-          "audio"       : "aud2",
-          "after"       : "spot1"
+          "_id": "spot2",
+          "position": "pos1",
+          "audio": "aud2",
+          "after": "spot1"
         }
       ]
     }
   ]
-}
+};
+
+export const config = { geo, audio, experience };
