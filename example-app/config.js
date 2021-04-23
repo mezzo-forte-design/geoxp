06a5e81f5d75d20d44a7c10db14a7e6ac9b1f7b5
francescocretti <francesco.cretti@gmail.com>
2021-04-24 00:57:22 +0200

    rename examples folders

---
+++ example-app/config.js
@@ -0,0 +1,66 @@
+// geographic configuration
+const geo = {
+  "positions": [
+    {
+      "id": "pos1",
+      "label": "pos1",
+      "lat": 45.116177,
+      "lon": 7.742615,
+      "radius": 10000,
+      "deadband": 20
+    }
+  ],
+  "default": {
+    "minAccuracy": 100,
+    "posDeadband": 10,
+    "playDistance": 20,
+    "fetchDistance": 1
+  }
+};
+
+// audio configuration
+const audio = {
+  "sounds": [
+    {
+      "id": "aud1",
+      "label": "aud1",
+      "url": "./audio/music1.mp3"
+    },
+    {
+      "id": "aud2",
+      "label": "aud2",
+      "url": "./audio/music2.mp3"
+    }
+  ],
+  "default": {
+    "test": "./audio/system/test.mp3",
+    "silence": "./audio/system/silence.mp3",
+    "visited": "./audio/system/visited.mp3"
+  }
+};
+
+// experience configuration
+const experience = {
+  "patterns": [
+    {
+      "label": "pattern1",
+      "enabled": true,
+      "overlap": false,
+      "spots": [
+        {
+          "id": "spot1",
+          "position": "pos1",
+          "audio": "aud1"
+        },
+        {
+          "id": "spot2",
+          "position": "pos1",
+          "audio": "aud2",
+          "after": "spot1"
+        }
+      ]
+    }
+  ]
+};
+
+export const config = { geo, audio, experience };
---
+++ example-patterns/orderedPattern.js
@@ -0,0 +1,97 @@
+/*
+Here's an example configuration for an ordered pattern.
+Three audios have to be played at three different locations, ordered from 1 to 3.
+*/
+
+import GeoXp from 'mezzoforte-geoXp';
+
+// step 1: geo configuration
+// maps all the relevant locations, provides info for geoFencing
+const geo = {
+  positions: [
+    {
+      id: 'pos1',
+      label: 'Torino - Piazza Castello',
+      lat: 45.0711383,
+      lon: 7.685391,
+      radius: 60, // meters
+      deadband: 40 // meters
+    },
+    {
+      id: 'pos2',
+      label: 'Torino - Piazza Vittorio Veneto',
+      lat: 45.0651445,
+      lon: 7.6929964
+      // if no geoFencing parameters are set, this position will use default values
+    },
+    {
+      id: 'pos3',
+      label: 'Torino - Gran Madre',
+      lat: 45.062657,
+      lon: 7.6972366,
+      radius: 50, // meters
+      fetch: 1.5 // starts loading content 1.5 * radius distance
+    }
+  ]
+};
+
+// step 2: audio configuration
+// maps all experience audio content
+const audio = {
+  sounds: [
+    {
+      id: "aud1",
+      label: "Duck",
+      url: "./audio/duck.mp3"
+    },
+    {
+      id: "aud2",
+      label: "Shouting",
+      url: "./audio/people_shouting.mp3"
+    },
+    {
+      id: "aud3",
+      label: "Talking",
+      url: "./audio/people_talking.mp3"
+    }
+  ]
+};
+
+// step 3: experience pattern configuration
+// putting things together
+const experience = {
+  patterns: [
+    // just one pattern is needed
+    {
+      id: 'exp1',
+      label: 'Ordered pattern',
+      spots: [
+        // first spot is in Piazza Castello (pos1), play duck (aud1)
+        {
+          id: 'sp1',
+          position: 'pos1',
+          audio: 'aud1'
+        },
+        // second spot is in Piazza Vittorio (pos2), play shouting (aud2)
+        // its content have to be played only if spot 1 has already been visited
+        {
+          id: 'sp2',
+          position: 'pos2',
+          audio: 'aud2',
+          after: 'sp1' // play content only after "sp1" has been visited
+        },
+        // third spot is at Gran Madre (pos3), play talking (aud3)
+        // its content have to be played only if spot 2 has already been visited
+        {
+          id: 'sp3',
+          position: 'pos3',
+          audio: 'aud3',
+          after: 'sp2' // play content only after "sp2" has been visited
+        },
+      ]
+    }
+  ]
+};
+
+// compose configuration object and create geoXp instance on configuration
+const geoXp = new GeoXp({ geo, audio, experience });
